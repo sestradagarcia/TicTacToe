950a9dcad7e2fc06ff3deb2fbcfa0d00fc6f8f7c
sestradagarcia <sestradagarcia>
2024-02-26 18:32:18 +0000

    added font to font folder

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { Lightning, Utils } from '@lightningjs/sdk'
+import Splash from './Splash.js'
+import Main from './Main.js'
+import Game from './Game.js'
 
 export default class App extends Lightning.Component {
   static getFonts() {
@@ -7,14 +10,97 @@ export default class App extends Lightning.Component {
 
   static _template() {
     return {
+      Background: {
+        rect: true,
+        color: 0xff404be3, // blue background colour (ARGB colour)
+        w: 1920, // Width of the background (adjust as needed)
+        h: 1080, // Height of the background (adjust as needed)
+      },
+      Logo: {
+        x: 100,
+        y: 100,
+        text: { text: 'TicTacToe', fontFace: 'pixel' },
+      },
       rect: true,
       color: 0xff000000,
-      w: 1920,
-      h: 1080,
+      Splash: {
+        type: Splash,
+        signals: { loaded: true },
+        alpha: 0,
+      },
+      Main: {
+        type: Main,
+        alpha: 0,
+        signals: { select: 'menuSelect' },
+      },
+      Game: {
+        type: Game,
+        alpha: 0,
+      },
     }
   }
 
+  _setup() {
+    this._setState('Splash')
+  }
+
   static _states() {
-    return []
+    return [
+      class Splash extends this {
+        $enter() {
+          this.tag('Splash').setSmooth('alpha', 1)
+        }
+
+        $exit() {
+          this.tag('Splash').setSmooth('alpha', 0)
+        }
+
+        loaded() {
+          this._setState('Main')
+        }
+      },
+      class Main extends this {
+        $enter() {
+          this.tag('Main').patch({
+            smooth: { alpha: 1, y: 0 },
+          })
+        }
+
+        $exit() {
+          this.tag('Main').patch({
+            smooth: { alpha: 0, y: 100 },
+          })
+        }
+
+        menuSelect({ item }) {
+          if (this._hasMethod(item.action)) {
+            return this[item.action]()
+          }
+        }
+
+        start() {
+          this._setState('Game')
+        }
+
+        // change focus path to main
+        // component which handles the remotecontrol
+        _getFocused() {
+          return this.tag('Main')
+        }
+      },
+      class Game extends this {
+        $enter() {
+          this.tag('Game').setSmooth('alpha', 1)
+        }
+
+        $exit() {
+          this.tag('Game').setSmooth('alpha', 0)
+        }
+
+        _getFocused() {
+          return this.tag('Game')
+        }
+      },
+    ]
   }
 }
--- src/Splash.js
+++ src/Splash.js
@@ -11,4 +11,18 @@ export default class Splash extends Lightning.Component {
       },
     }
   }
+
+  _init() {
+    this._pulse = this.tag('Logo').animation({
+      duration: 4,
+      repeat: 0,
+      actions: [{ p: 'alpha', v: { 0: 0, 0.5: 0.5, 1: 0 } }],
+    })
+
+    this._pulse.on('finish', () => {
+      this.signal('loaded')
+    })
+
+    this._pulse.start()
+  }
 }
